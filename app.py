ce0b9822f4731f4d4a78622a8d1d3622d0f7cf0e
kshitijvashisth <20400812-kshitijvashisth@users.noreply.replit.com>
2023-11-11 23:07:41 +0000

    trying web app development

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello Worldbhosdi!'
+
+@app.route('/about')
+def harry():
+    name = 'Kshitij'
+    return render_template('about.html', name = name)
+if __name__ == '__main__':
+  app.run(host='0.0.0.0',debug=True)
