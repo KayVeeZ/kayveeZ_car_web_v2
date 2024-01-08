ac1cadaefc00d632dc06d28caa11a9ff719d4481
KayVeeZ <125533246+KayVeeZ@users.noreply.github.com>
2024-01-08 23:02:33 +0000

    dynamic pages for each job, aesthetic improvements

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 
 app = Flask(__name__)
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
   jobs_list = load_jobs_from_db()
-  return render_template('index.html', jobs = jobs_list, company_name = 'KayVeeZ')
+  return render_template('index.html', jobs = jobs_list)
 
 @app.route('/about')
 def about():
@@ -18,5 +18,14 @@ def jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
 
+@app.route('/job/<id>')
+def show_job(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not Found", 404
+  return render_template('jobpage.html', job = job)
+  return render_template('jobpage.html' , job = job)
+
+  
 if __name__ == '__main__':
   app.run(host='0.0.0.0',debug=True)
