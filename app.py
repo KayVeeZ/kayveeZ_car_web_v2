4b7c7ed97e32749311b3c0b970674b03c3bbb24c
KayVeeZ <125533246+KayVeeZ@users.noreply.github.com>
2024-01-10 22:11:23 +0000

    some minor changes

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from database import load_jobs_from_db, load_job_from_db
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def home_page():
   jobs_list = load_jobs_from_db()
   return render_template('index.html', jobs = jobs_list, company_name = 'KayVeeZ')
 
